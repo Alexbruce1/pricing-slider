7d25ed986b0b672c3e2067bb99a5e910f235133c
Alex Bruce <brucealmightyalex@yahoo.com>
2018-07-07 14:37:49 -0600

    First commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var switchOne = document.getElementById('switch-one');
+var switchTwo = document.getElementById('switch-two');
+var outputPrice = document.getElementById('output-price');
+var sliderCheckbox = document.getElementById('slider-checkbox');
+var output = document.querySelector('.output');
+
+
+switchOne.addEventListener('click', function() {
+  // if (sliderCheckbox.checked = true) {
+  //   sliderCheckbox.checked = false;
+  // } else {
+  //   sliderCheckbox.checked = true;
+  //   console.log('no')
+  // }
+})
